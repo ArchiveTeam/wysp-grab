f8ee760010b99e3ce05301ec09dc16245e7b2465
OrIdow6 <68304414+OrIdow6@users.noreply.github.com>
2023-07-21 05:38:59 -0700

    Allow main image URLs with non-newline whitespace in them

--- handlers/submission.lua
+++ handlers/submission.lua
@@ -11,9 +11,9 @@ module.get_urls = function(file, url, is_css, iri)
     end
     -- Queue the image URL
     if get_body():match('sapImage') then -- Only image posts
-        queue_request({url=get_body():match('<img class="sapImage" src="(https?://%S-)"')}, retry_common.only_retry_handler(5, {200}), false)
+        queue_request({url=get_body():match('<img class="sapImage" src="(https?://[^\n"<>]-)"')}, retry_common.only_retry_handler(5, {200}), false)
         -- Also the "full size" image
-        local full_size = get_body():match('<a href="(https?://%S-)" class="theaterStatlineLink" target="_blank">Full size</a>')
+        local full_size = get_body():match('<a href="(https?://[^\n]-)" class="theaterStatlineLink" target="_blank">Full size</a>')
         queue_request({url=full_size}, retry_common.only_retry_handler(5, {200}), false)
     end
 
--- pipeline.py
+++ pipeline.py
@@ -58,7 +58,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20230720.01'
+VERSION = '20230721.01'
 USER_AGENT = 'Mozilla/5.0 (Incompatible; Archiveteam (https://wiki.archiveteam.org/; communicate at https://webirc.hackint.org/#ircs://irc.hackint.org/#archiveteam)'
 TRACKER_ID = 'wysp'
 TRACKER_HOST = 'legacy-api.arpa.li'
